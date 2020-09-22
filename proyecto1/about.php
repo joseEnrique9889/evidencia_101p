263b85e01b356747b839aaaecb493f4e45b164d6
joseEnrique9889 <71733840+joseEnrique9889@users.noreply.github.com>
2020-09-22 17:11:41 -0500

    Add files via upload

---
+++ proyecto1/about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Acerca de</title>
+</head>
+<body> 
+	<h1>este es mo acerca de</h1>
+
+</body>
+</html>
---
+++ proyecto1/barranav.php
@@ -0,0 +1,17 @@
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h5 class="text-white h4">NOMBRE DE LA EMPRESA</h5>
+      
+      
+     <a class="nav-link" href="about.php">A cerca de</a> 
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    
+    </button>
+
+  </nav>
+</div>
